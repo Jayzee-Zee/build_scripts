833002835b8f6d22e57409d63f09f42119c406a9
Jayzee-Zee <i.am.hipop@gmail.com>
2024-08-30 15:14:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,9 @@ git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b PixelProject
 # Set up build environment
 export BUILD_USERNAME=Jayzee-Zee 
 export BUILD_HOSTNAME=crave
-#export TARGET_PRODUCT=lineage_earth
+export TARGET_PRODUCT=lineage_earth
+export TARGET_BUILD_VARIANT=userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 #export TARGET_RELEASE=ap2a
 export TZ=Asia/Jakarta
 #export RISING_MAINTAINER=Jayzee
