275e2ea86a4a781ca1ce08e0640b8c240fc638a5
Jayzee-Zee <i.am.hipop@gmail.com>
2024-08-30 17:47:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@ rm -rf out/target/product/earth/*
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 repo init -u https://github.com/The-Pixel-Project/manifest.git -b fourteen-qpr3 --git-lfs --depth=1
 #clone dev tree
-git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b PixelProject .repo/local_manifests &&
+git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b PixelProject --depth=1
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth -b lineage-21 --depth=1
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 --depth=1
+git clone https://github.com/Jayzee-Zee/android_kernel_xiaomi_earth -b nonksu --depth=1
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 --depth=1
+git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 --depth=1
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
