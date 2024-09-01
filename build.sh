7c0464491712e2a1c2cf8cc44c7353fe9ff38654
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-01 16:29:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,22 +7,22 @@ rm -rf hardware/xiaomi
 rm -rf hardware/mediatek
 rm -rf frameworks/base
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
-repo init -u https://github.com/AOSPA/manifest.git -b uvite --git-lfs --depth=1
+repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr --git-lfs --depth=1
 #clone dev tree
-git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b AOSPA device/xiaomi/earth 
+git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b CipherOS device/xiaomi/earth 
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth -b lineage-21 vendor/xiaomi/earth
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
 git clone https://github.com/Jayzee-Zee/android_kernel_xiaomi_earth -b nonksu kernel/xiaomi/earth
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek
-git clone https://github.com/AOSPA/android_frameworks_base -b uvite frameworks/base
+git clone https://github.com/CipherOS/android_frameworks_base -b fourteen-qpr frameworks/base
 # Sync the repositories
-/opt/crave/resync.sh  && 
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)  && 
 # Set up build environment
 chmod +x vendor/lineage/config/*.mk
 chmod +x vendor/lineage/*
 export BUILD_USERNAME=Jayzee-Zee
-export TARGET_PRODUCT=aospa_earth
+export TARGET_PRODUCT=cipher_earth
 export TARGET_BUILD_VARIANT=userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export BUILD_HOSTNAME=crave
@@ -33,8 +33,8 @@ export KBUILD_USERNAME=Jayzee-Zee
 export KBUILD_HOSTNAME=crave
 source build/envsetup.sh
 
-lunch aospa_earth-ap2a-userdebug
-lunch aospa_earth-userdebug
+lunch cipher_earth-ap2a-userdebug
+lunch cipher_earth-userdebug
 
 
 m bacon
