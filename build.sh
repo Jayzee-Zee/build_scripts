adb7e707d96c4229565dfdbbf1027fc17daea18c
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-05 22:47:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,11 @@ repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr --
 #clone dev tree
 git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b CipherOS .repo/local_manifests &&
 # Sync the repositories
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)  && 
+/opt/crave/resync.sh  &&
 # Set up build environment
-chmod +x vendor/lineage/config/*.mk
-chmod +x vendor/lineage/*
 export BUILD_USERNAME=Jayzee-Zee
-export TARGET_PRODUCT=cipher_earth
-export TARGET_BUILD_VARIANT=userdebug
+#export TARGET_PRODUCT=cipher_earth
+#export TARGET_BUILD_VARIANT=userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export BUILD_HOSTNAME=crave
 #export TARGET_PRODUCT=lineage_earth
@@ -20,9 +18,7 @@ export KBUILD_USERNAME=Jayzee
 export KBUILD_HOSTNAME=crave
 source build/envsetup.sh
 
-lunch cipher_earth-ap1a-userdebug
-lunch cipher_earth-userdebug
-
+breakfast earth userdebug
 
 m bacon
  
