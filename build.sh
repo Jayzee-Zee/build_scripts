029f657f5ecffb7172c61002b8f86c11eba1ae7e
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-05 22:44:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,8 @@
-rm -rf out/target/product/earth/*
-rm -rf device/xiaomi/earth
-rm -rf vendor/xiaomi/earth
-rm -rf device/mediatek/sepolicy_vndr
-rm -rf kernel/xiaomi/earth
-rm -rf hardware/xiaomi
-rm -rf hardware/mediatek
-rm -rf frameworks/base
+rm -rf out/target/product/earth/*.zip
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr --git-lfs --depth=1
 #clone dev tree
-git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b CipherOS device/xiaomi/earth 
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth -b lineage-21 vendor/xiaomi/earth
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
-git clone https://github.com/Jayzee-Zee/android_kernel_xiaomi_earth -b nonksu kernel/xiaomi/earth
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek
-git clone https://github.com/CipherOS/android_frameworks_base -b fourteen-qpr frameworks/base
+git 
 # Sync the repositories
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)  && 
 # Set up build environment
