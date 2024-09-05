793e63a2a14bba0893d6ff273521755fd303aada
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-06 01:03:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,12 +7,8 @@ git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b CipherOS .re
 /opt/crave/resync.sh  &&
 # Set up build environment
 export BUILD_USERNAME=Jayzee-Zee
-#export TARGET_PRODUCT=cipher_earth
-#export TARGET_BUILD_VARIANT=userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export BUILD_HOSTNAME=crave
-#export TARGET_PRODUCT=lineage_earth
-#export TARGET_RELEASE=ap2a
 export TZ=Asia/Jakarta
 export KBUILD_USERNAME=Jayzee-Zee
 export KBUILD_HOSTNAME=crave
@@ -24,5 +20,4 @@ echo "Im crave sir I will start beelding now Just hope it doesn't fail lol"
 
 m bacon
  
-# beelding
 
