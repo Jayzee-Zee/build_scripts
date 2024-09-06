8f7ee445424abb1e19a1df4e032354343d27076b
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-06 12:03:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,4 @@
-cd ~/
-wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-# add Android SDK platform tools to path
-if [ -d "$HOME/platform-tools" ] ; then
-    PATH="$HOME/platform-tools:$PATH"
-fi
-source ~/.profile
-cd ~/
-git clone https://github.com/akhilnarang/scripts
-cd scripts
-./setup/android_build_env.sh
-cd ~/
-sudo mkdir -p ~/bin
-sudo mkdir -p ~/android
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-chmod a+x ~/bin/repo
-git config --global user.email "salitagwapo@gmail.com"
-git config --global user.name "Jayzee"
-pkg upgrade
-sudo mkdir /android
-cd /android
+
 rm -rf out/target/product/earth/*.zip
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr --git-lfs --depth=1
