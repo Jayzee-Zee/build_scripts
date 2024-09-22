298f81be2eca6a29332e56ff69e58956c3372f3e
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-22 17:34:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 rm -rf out/target/product/earth/*
 rm -rf hardware/xiaomi
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
-repo init -u https://github.com/AlphaDroid-Project/manifest.git -b alpha-14 --git-lfs --depth=1
+repo init  -u https://github.com/ProjectBlaze/manifest -b 14 --depth=1
 #clone dev tree
 #git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b Alpha .repo/local_manifests &&
 # Sync the 
 #repositories
-git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b Alpha device/xiaomi/earth 
+git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b Blaze device/xiaomi/earth 
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth -b lineage-21 vendor/xiaomi/earth
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
 git clone https://github.com/Jayzee-Zee/android_kernel_xiaomi_earth -b nonksu kernel/xiaomi/earth
@@ -22,7 +22,7 @@ export TZ=Asia/Jakarta
 source build/envsetup.sh
  
 # Build the ROM
-lunch lineage_earth-ap2a-userdebug
+lunch blaze_earth-ap2a-userdebug
 
 #beelding
 
