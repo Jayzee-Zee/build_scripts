186e9a01d51c4e34e7c7874a77a67186f06f8507
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-28 19:56:22 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,13 +12,14 @@ repo init -u https://github.com/The-Pixel-Project/manifest.git -b fourteen-qpr3 
 #git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
 #git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek
 #git clone https://github.com/The-Pixel-Project/frameworks_base -b fourteen-qpr3 frameworks/base
+git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b Pixel .repo/local_manifests &&
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
 export BUILD_USERNAME=Jayzee-Zee 
 export BUILD_HOSTNAME=crave
-export TARGET_PRODUCT=lineage_earth
-export TARGET_BUILD_VARIANT=userdebug
+#export TARGET_PRODUCT=lineage_earth
+#export TARGET_BUILD_VARIANT=userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 #export TARGET_RELEASE=ap2a
 export TZ=Asia/Jakarta
@@ -28,9 +29,7 @@ source build/envsetup.sh
 # Build the ROM
 chmod +x vendor/lineage/config/common_full_phone.mk
 chmod +x vendor/lineage/config/*
-lunch lineage_earth-userdebug
-
-#lunch lineage_earth-ap2a
+lunch lineage_earth-userdebug || lunch lineage_earth-ap2a-userdebug
 
 #beelding
 make bacon -j$(nproc --all)
