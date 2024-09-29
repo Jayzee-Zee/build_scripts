293fbcd02d2116828db11449aa70c95e3c3bed02
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-29 11:43:02 +0800

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,12 +1,11 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo init -u romsource -b branch --git-lfs
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+git clone yourmanifest -b branch .repo/local_manifests
 
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
-riseup mi439 userdebug 
-make installclean
-rise b
+lunch lineage_earth-userdebug #edit this according to your rom build commands
+make bacon #also edit this if needed
