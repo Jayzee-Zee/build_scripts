33059fad69b87fc26614f83ee299374d30c28868
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-29 21:59:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
-rm -rf out/target/product/earth/*
-rm -rf hardware/xiaomi
-rm -rf hardware/mediatek
-rm -rf frameworks/base
-rm -rf .repo/local_manifests/  && # Clone local_manifests repository
-repo init -u https://github.com/The-Pixel-Project/manifest.git -b fourteen-qpr3 --git-lfs --depth=1
+#rm -rf out/target/product/earth/*
+#rm -rf hardware/xiaomi
+#rm -rf hardware/mediatek
+#rm -rf frameworks/base
+#rm -rf .repo/local_manifests/  && # Clone local_manifests repository
+#repo init -u https://github.com/The-Pixel-Project/manifest.git -b fourteen-qpr3 --git-lfs --depth=1
 #clone dev tree
-git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b Pixel device/xiaomi/earth 
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth -b lineage-21 vendor/xiaomi/earth
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
-git clone https://github.com/Jayzee-Zee/android_kernel_xiaomi_earth -b nonksu kernel/xiaomi/earth
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek &&
+#git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b Pixel device/xiaomi/earth 
+#git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth -b lineage-21 vendor/xiaomi/earth
+#git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
+#git clone https://github.com/Jayzee-Zee/android_kernel_xiaomi_earth -b nonksu kernel/xiaomi/earth
+#git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+#git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek &&
 #git clone https://github.com/The-Pixel-Project/frameworks_base -b fourteen-qpr3 frameworks/base
 #git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b Pixel .repo/local_manifests &&
 # Sync the repositories
-/opt/crave/resync.sh  && 
+#/opt/crave/resync.sh  && 
 # Set up build environment
 export BUILD_USERNAME=Jayzee-Zee 
 export BUILD_HOSTNAME=crave
