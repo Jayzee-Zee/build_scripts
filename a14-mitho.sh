24ed96a52b62944a7c86db2854d93ef0b975b507
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-29 11:44:54 +0800

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -2,10 +2,14 @@ rm -rf .repo/local_manifests/
 #repo init rom
 repo init -u romsource -b branch --git-lfs
 #Local manifests
-git clone yourmanifest -b branch .repo/local_manifests
+git clone yourmanifest -b branch .repo/local_manifests &&
 
 #build
 /opt/crave/resync.sh
-. build/envsetup.sh
+source build/envsetup.sh
+export BUILD_USERNAME=Jayzee-Zee 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
+export TZ=Asia/Jakarta
 lunch lineage_earth-userdebug #edit this according to your rom build commands
 make bacon #also edit this if needed
