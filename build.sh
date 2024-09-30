c8078a0392be9b8007a0ec6f03248a7d4ff78906
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-30 16:12:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b CipherOS .re
 # Sync the repositories
 /opt/crave/resync.sh &&
 # Set up build environment
+source build/envsetup.sh
 export BUILD_USERNAME=Jayzee-Zee
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export BUILD_HOSTNAME=crave
 export TZ=Asia/Jakarta
 export KBUILD_USERNAME=Jayzee-Zee
 export KBUILD_HOSTNAME=crave
-source build/envsetup.sh
-
+chmod +x vendor/cipher/config/common_full_phone.mk
 lunch cipher_earth-userdebug || lunch cipher_earth-ap1a-userdebug || lunch cipher_earth-ap2a-userdebug || breakfast earth userdebug || brunch earth userdebug
 
 echo "Im crave sir I will start beelding now Just hope it doesn't fail lol"
