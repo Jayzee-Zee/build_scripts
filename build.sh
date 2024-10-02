443c23c8323817719ecc13c52ceea30e81918fcf
Jayzee-Zee <i.am.hipop@gmail.com>
2024-10-02 14:07:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ rm -rf device/xiaomi/earth
 rm -rf device/mediatek/sepolicy_vndr
 rm -rf kernel/xiaomi/earth
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
-repo init -u https://github.com/The-Pixel-Project/manifest.git -b fourteen-qpr3 --git-lfs --depth=1
+repo init -u https://github.com/LMODroid/manifest.git -b fourteen --git-lfs --depth=1
 #clone dev tree
-git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b Pixel device/xiaomi/earth 
+git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b LMO device/xiaomi/earth 
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth -b lineage-21 vendor/xiaomi/earth
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
 git clone https://github.com/Jayzee-Zee/android_kernel_xiaomi_earth -b nonksu kernel/xiaomi/earth
@@ -29,14 +29,18 @@ export TZ=Asia/Jakarta
 #export RISING_MAINTAINER=Jayzee
 source build/envsetup.sh || sudo bash build/envsetup.sh
 
+rm -rf hardware/xiaomi
+rm -rf hardware/mediatek
+rm -rf device/mediatek/sepolicy_vndr
+
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek
  
 # Build the ROM
-chmod 777 vendor/aosp/config/common_full_phone.mk
-chmod 777 vendor/aosp/config/*.mk
-lunch aosp_earth-userdebug || lunch aosp_earth-ap2a-userdebug || breakfast earth userdebug
+chmod 777 vendor/lmodroid/config/common_full_phone.mk
+chmod 777 vendor/lmodroid/config/*.mk
+lunch lmodroid_earth-userdebug || lunch lmodroid_earth-ap2a-userdebug || breakfast earth userdebug
 
 #beelding
 make bacon -j$(nproc --all)
